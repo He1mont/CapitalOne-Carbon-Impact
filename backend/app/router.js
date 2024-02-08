7fcaa3b393da95642c85faa9cb83815962debac9
Guosheng Su <scygs2@nottingham.ac.uk>
2024-02-08 21:06:30 +0000

    test(backend): fix bugs in `test/controller/transaction.js`

--- backend/app/router.js
+++ backend/app/router.js
@@ -6,9 +6,9 @@ module.exports = app => {
   router.get('/', controller.home.index);
   router.get('/account/create-random', controller.account.createRandom);
   router.get('/account/get-all', controller.account.getAll);
+  router.get('/account/get-by-id/:id', controller.account.getByID);
+  router.get('/account/get-by-email/:email', controller.account.getByEmail);
   router.get('/transaction/get-all/:id', controller.transaction.getAll);
   router.get('/transaction/get-by-id/:accountID/:transactionID', controller.transaction.getByID);
   router.get('/transaction/group-by-date/:id', controller.transaction.groupByDate);
-  router.get('/account/get-by-id/:id', controller.account.getByID);
-  router.get('/account/get-by-email/:email', controller.account.getByEmail);
 };
