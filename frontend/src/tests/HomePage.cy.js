de2d7852968e081bbe4dfb655ff4238f6b6a6441
Guosheng Su <scygs2@nottingham.ac.uk>
2024-02-19 20:41:04 +0000

    test(frontend): add test for Homepage

--- frontend/src/tests/HomePage.cy.js
+++ frontend/src/tests/HomePage.cy.js
@@ -1,10 +1,19 @@
-import React from 'react'
-import HomePage from '../pages/HomePage'
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { mount } from '@cypress/react';
+import HomePage from '../pages/HomePage'; 
 
-describe('<HomePage />', () => {
-  it('mounts', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<HomePage />)
-    cy.get('[data-cy=counter]').should('have.text', '0')
-  })
-})
+describe('HomePage Component', () => {
+  it('successfully renders', () => {
+    mount(
+      <BrowserRouter>
+        <HomePage />
+      </BrowserRouter>
+    );
+    
+    cy.get('.login-btn').should('exist');
+    cy.contains('Your Carbon Impact').should('exist');
+    cy.get('.low-bar-btn').should('have.length', 3);
+    cy.get('.footer').should('contain', '© 2023-2024 Team7. All rights reserved.');
+  });
+});
