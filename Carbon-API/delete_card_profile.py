6e381db3e9e889350e8e634dcbf9a503261e6da7
Tanaya <psytp5@nottingham.ac.uk>
2024-02-24 17:57:55 +0000

    feat: carbon API updates when adding data from js

--- Carbon-API/delete_card_profile.py
+++ Carbon-API/delete_card_profile.py
@@ -1,8 +1,8 @@
 import json
 import requests
 
-PROGRAM_UUID = "5010a4d0-bd3e-4c08-83f6-7ca0a3a33098"
-CARBON_API_KEY = "Q1C3WbWtKuxKa3a2DDC3Q"
+PROGRAM_UUID = "ddd7027e-2032-4fff-a721-565ac87e7869" #"5010a4d0-bd3e-4c08-83f6-7ca0a3a33098"
+CARBON_API_KEY = "sQyPyTxcWvlFiLWFjmUlA" #"Q1C3WbWtKuxKa3a2DDC3Q"
 
 Carbon_API_headers = {
     'Authorization': f'Bearer {CARBON_API_KEY}',
