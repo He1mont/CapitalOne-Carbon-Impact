628a7bf5f2bbaf6233d1798af7437d879d8a47ad
psyfu1 <psyfu1@nottingham.ac.uk>
2024-02-27 19:25:53 +0000

    Added: Test Goals.cy.js

---
+++ frontend/src/tests/Goals.cy.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import { mount } from '@cypress/react';
+import { MemoryRouter } from 'react-router-dom';
+import moment from 'moment'; 
+import Goals from '../pages/Goals';
+import styles from '../assets/styles/Goals.module.css';
+
+describe('<Goals />', () => {
+  it('successfully renders', () => {
+    mount(
+      <MemoryRouter>
+        <Goals />
+      </MemoryRouter>
+    );
+
+       // Rendering
+       cy.contains('Carbon Goals').should('exist');
+       
+
+       
+  });
+  
+  it('renders the month selector with correct initial month', () => {
+    mount(
+      <MemoryRouter>
+        <Goals />
+      </MemoryRouter>
+    );
+
+    // Checking format of currentMonth existing
+    const currentMonth = moment().format('MMM YYYY'); 
+    cy.contains(currentMonth).should('exist');
+  });
+  it('changes the month when clicking on month selector arrows', () => {
+    mount(
+      <MemoryRouter>
+        <Goals />
+      </MemoryRouter>
+    );
+  
+    // Get the initial month
+    const initialMonth = moment().format('MMM YYYY');
+  
+    // Click on the left arrow to decrease the month
+    cy.get(`.${styles.month_select_btn}`).first().click();
+  
+    // Assert that the month has changed
+    cy.contains(initialMonth).should('not.exist');
+  
+    // Click on the right arrow to increase the month
+    cy.get(`.${styles.month_select_btn}`).last().click();
+  
+    // Assert that the month has changed back to the initial month
+    cy.contains(initialMonth).should('exist');
+  });
+  
+
+  /*it('renders the CarbonUseCircle component with correct carbon emission and goal emissions', () => {
+    mount(
+      <MemoryRouter>
+        <Goals />
+      </MemoryRouter>
+    );
+    cy.wait(1000);
+    // Ensure that the CarbonUseCircle component renders correctly with given props
+    cy.get(`.${styles.head_img}`).within(() => {
+      cy.contains('1600 kgco2').should('exist');
+      cy.contains('2000 kgco2').should('exist');
+    });
+  });*/
+  it('navigates to home page on logo click', () => {
+    mount(
+      <MemoryRouter>
+        <Goals />
+      </MemoryRouter>
+    );
+
+    // Click on the logo to navigate to the home page
+    cy.get(`.${styles.head_img}`).click();
+
+    // URL has changed to the home page
+   // cy.url().should('eq', 'http://localhost:8080/');
+  });
+
+  it('navigates to profile page on user profile icon click', () => {
+    mount(
+      <MemoryRouter>
+        <Goals />
+      </MemoryRouter>
+    );
+  
+    // Click on the user profile icon
+    cy.get(`.${styles.mid_high_profile}`).click();
+  
+    // URL has changed to the profile page
+   // cy.url().should('eq', 'http://localhost:8080/')
+  });
+
+
+  it('renders lower section content', () => {
+    mount(
+      <MemoryRouter>
+        <Goals />
+      </MemoryRouter>
+    );
+  
+    // Ensure that the lower section content is rendered
+    cy.get(`.${styles.low_bar}`).should('exist');
+  });
+
+  it('redirects unauthenticated users to login page', () => {
+   // Clear any stored authentication tokens
+    cy.clearLocalStorage(); 
+    
+
+  });
+  
+
+});
--- frontend/src/tests/Transactions.cy.js
+++ frontend/src/tests/Transactions.cy.js
@@ -12,20 +12,20 @@ describe('Transactions Page', () => {
     );
   });
 
+
   it('successfully renders the Transactions page components', () => {
-    cy.contains('View Transactions').should('exist');
-    cy.contains('Carbon Impact (kgco2)').should('exist');
+   cy.contains('View Transactions').should('exist');
+   cy.contains('Carbon Impact (kgco2)').should('exist');
     cy.contains('Date').should('exist');
     cy.contains('Category').should('exist');
     cy.contains('Amount').should('exist');
-    cy.contains('Download transactions').should('exist');
     cy.contains('Description').should('exist');
     cy.contains('1000').should('exist');
     cy.contains('estimate').should('exist');
     cy.contains('kgco2').should('exist');
-    cy.contains('Download transactions').should('exist');
     cy.contains('You need to login').should('exist');
     cy.contains('Feb 2024').should('exist');
+  // cy.get('.head_img').should('exist');
   });
   it('displays the logo image and navigates on click', () => {
     // Assert that the logo image exists
@@ -46,21 +46,11 @@ describe('Transactions Page', () => {
      // cy.get('.mid_box').should('be.visible');
       cy.contains('1000 kgco2').should('exist');
     });
-
-    /*it('allows month selection and reflects changes', () => {
-      // Click on the button to decrease the month
-      cy.get('.month_select_btn').eq(0).click();
-   
-    });*/
-   /* it('checks for the presence of a specific paragraph element in Mid component', () => {
-      // Visit the page or navigate to the Transactions page where the Mid component is rendered
-     // cy.visit('../pages/Transactions');
-
-      // Assert
-      cy.get('.mid-box-txt-line')
-        .should('exist')
-        .and('have.text', 'Find out how your spending habits affect the planet');
-    });*/
+    it('allows searching for transactions', () => {
+    
+      // Check if the transactions are filtered correctly
+      cy.contains('Transaction').should('exist');
+    });
     
     
   });
