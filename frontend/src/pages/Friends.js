b8defaccbd65a63ed587316eefa01efb551d0642
Yanqian WANG <scyyw13@exmail.nottingham.ac.uk>
2024-04-16 23:22:38 +0100

    feat: add a delete button to handle deleting friends in the Friends page

--- frontend/src/pages/Friends.js
+++ frontend/src/pages/Friends.js
@@ -1,5 +1,5 @@
 // Friends.js
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import styles from '../assets/styles/Friends.module.css';
 import { useHistory, useLocation } from 'react-router-dom';
 // helper functions
@@ -67,9 +67,9 @@ class Leaderboard extends Component {
         this.state = {
             friendList: [],
             newFriend: '',
+            selectionModel: [],
         };
         this.handleChange = this.handleChange.bind(this);
-        this.handleAddFriend = this.handleAddFriend.bind(this);
         this.removeFriend = this.removeFriend.bind(this);
     }
     // initialize and display the friendList
@@ -108,12 +108,21 @@ class Leaderboard extends Component {
         }
     }
     // remove a friend by calling backend API
-    removeFriend = async(friend) => {
-        await API.deleteFollowing(this.props.userID, friend.accountID);
-        this.setState(prevState => ({
-            friendList: prevState.friendList.filter(followingUser => followingUser !== friend)
+    removeFriend = async () => {
+        const { selectionModel } = this.state;
+    
+        selectionModel.forEach(async (friend) => {
+            await API.deleteFollowing(this.props.userID, friend.accountID);
+        });
+    
+        // update friendList and selectionModel
+        this.setState((prevState) => ({
+            friendList: prevState.friendList.filter((followingUser) => !prevState.selectionModel.includes(followingUser)),
         }));
-    }
+        this.setState({ selectionModel: [] });
+    };
+    
+
     // update search frame
     handleChange = (event) => {
         this.setState({ newFriend: event.target.value });
@@ -125,6 +134,32 @@ class Leaderboard extends Component {
             this.addFriend();
         }
     }
+
+    handleSingleCellClick = (newSelection) => {
+        this.setState((prevState) => {
+            // check if exist
+            const index = prevState.selectionModel.findIndex((item) => item.accountID === newSelection.row.accountID);
+            if (index === -1) {
+                return {
+                    selectionModel: [...prevState.selectionModel, newSelection.row],
+                };
+            } else {
+                const updatedSelection = prevState.selectionModel.filter
+                    ((item) => item.accountID !== newSelection.row.accountID);
+                return {
+                    selectionModel: updatedSelection,
+                };
+            }
+        }, () => {
+            console.log(this.state.selectionModel);
+        });
+    };
+
+    handleColumnClick = () => {
+        console.log(111)
+
+    }
+
     render() {
         const followingUsers = this.state.friendList;
         const columns = [
@@ -152,6 +187,9 @@ class Leaderboard extends Component {
                     <button className={styles._dropbtn} onClick={this.addFriend}>
                         Confirm
                     </button>
+                    <button className={styles._dropbtn} onClick={this.removeFriend}>
+                        Delete
+                    </button>
                 </div>
                 <div className={styles.leaderboard_container}>
                     {this.state.friendList.length === 0 ? (
@@ -168,6 +206,8 @@ class Leaderboard extends Component {
                                 }}
                                 pageSizeOptions={[5, 10]}
                                 checkboxSelection
+                                onCellClick={this.handleSingleCellClick}
+                                onColumnHeaderClick={this.handleColumnClick}
                             />
                         </div>
                     )}
