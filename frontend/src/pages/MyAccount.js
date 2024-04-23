dd5e3990f4f28d521737349528e498daf593a704
Benji Goringe <benjigoringe@benjis-mbp.home>
2024-04-23 14:24:53 +0100

    feat: added headings and sample data to MyAccount

--- frontend/src/pages/MyAccount.js
+++ frontend/src/pages/MyAccount.js
@@ -21,6 +21,69 @@ function Mid({name, id}) {
             <div className={styles.midHigh} />
             <div className={styles.midLow} />
             <div className={styles.midCenter}>
+                <div className={styles.centerContainer}>
+                    <div className={styles.titleText}>My Account</div>
+                    <div className={styles.sectionHeader}>Personal</div>
+                    <table className={styles.AccInfoTable}>
+                        <tr>
+                            <td className={styles.AccTableHeads}> Firstname(s): </td>
+                            <td className={styles.AccTableHeads}> Phone Number: </td>                          
+                        </tr>
+                        <tr>
+                            <td> Benji </td>
+                            <td> +44 1234 567890</td>                          
+                        </tr>
+                        <br/>
+                        <tr>
+                            <td className={styles.AccTableHeads}> Lastname(s): </td>
+                            <td className={styles.AccTableHeads}> Email Address: </td>                          
+                        </tr>
+                        <tr>
+                            <td> Goringe</td>
+                            <td> email@email.com</td>                          
+                        </tr>
+                        <br/>
+                        <tr>
+                            <td className={styles.AccTableHeads}> Account ID: </td>
+                            <td className={styles.AccTableHeads}> Home Address: </td>                          
+                        </tr>
+                        <tr>
+                            <td> 12345678</td>
+                            <td> house, AB1 2CD</td>                          
+                        </tr>
+                    </table>
+                    <div className={styles.sectionHeader}>Transactions</div>
+                    <table className={styles.AccInfoTable}>
+                        <tr>
+                            <td className={styles.AccTableHeads}> First Transaction: </td>
+                            <td className={styles.AccTableHeads}> Completed Transactions</td>                          
+                        </tr>
+                        <tr>
+                            <td> 03/08/2023</td>
+                            <td> 48</td>                          
+                        </tr>
+                        <br/>
+                        <tr>
+                            <td className={styles.AccTableHeads}> Most Recent Transaction: </td>
+                            <td className={styles.AccTableHeads}> Value of Transactions</td>                          
+                        </tr>
+                        <tr>
+                            <td> 21/04/2024</td>
+                            <td> $2,123</td>                          
+                        </tr>
+                    </table>
+                    <div className={styles.sectionHeader}>Additional Options</div>
+                    <table className={styles.AccInfoTable}>
+                        <tr>
+                            <td className={styles.AccTableHeads}> Colour Correction: </td>
+                            <td className={styles.AccTableHeads}> Primary Currency: </td>                          
+                        </tr>
+                        <tr>
+                            <td> 03/08/2023</td>
+                            <td> 48</td>                          
+                        </tr>
+                    </table>
+                </div>
                 
             </div>
         </div>
